4169c58b68f654c341e019818ef846927818f2ac
teranga@wiley.com <95670397+tharinduwiley@users.noreply.github.com>
2022-06-07 10:03:03 +0530

    init git

---
+++ src/main/java/com/bsf/wallet/WalletTransferApplication.java
@@ -0,0 +1,13 @@
+package com.bsf.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletTransferApplication.class, args);
+	}
+
+}
