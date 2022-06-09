f509e97606f0208910416354867aeebcf388c8e3
teranga@wiley.com <95670397+tharinduwiley@users.noreply.github.com>
2022-06-09 20:12:10 +0530

    add in memory h2 database

--- src/test/java/com/bsf/wallet/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/bsf/wallet/service/impl/AccountServiceImplTest.java
@@ -1,15 +1,34 @@
 package com.bsf.wallet.service.impl;
 
+import com.bsf.wallet.dto.response.AccountDetail;
+import com.bsf.wallet.entity.Account;
+import com.bsf.wallet.repository.AccountRepository;
+import com.bsf.wallet.service.AccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by: Tharindu Eranga
  * Date: 08 Jun 2022
  **/
+@SpringBootTest
+@ActiveProfiles("test")
 class AccountServiceImplTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private AccountRepository accountRepository;
 
     @BeforeEach
     void setUp() {
@@ -17,5 +36,12 @@ class AccountServiceImplTest {
 
     @Test
     void getAccounts() {
+        List<Account> accountsSaved = Arrays.asList(
+                new Account(1L, "John", "323232", BigDecimal.TEN, LocalDateTime.now(), 1L),
+                new Account(2L, "Anna", "898787", BigDecimal.TEN, LocalDateTime.now(), 1L)
+        );
+        accountRepository.saveAll(accountsSaved);
+        List<AccountDetail> fetchedAccounts = accountService.getAccounts();
+        assertEquals(accountsSaved.size(), fetchedAccounts.size());
     }
 }
