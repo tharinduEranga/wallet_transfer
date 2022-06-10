0f409aaf25b7c27af5c7dd58c11a4b7b5debe9ff
teranga@wiley.com <95670397+tharinduwiley@users.noreply.github.com>
2022-06-10 18:52:11 +0530

    updated account integration test data issue

--- src/test/java/com/bsf/wallet/integration/service/impl/AccountServiceImplTest.java
+++ src/test/java/com/bsf/wallet/integration/service/impl/AccountServiceImplTest.java
@@ -3,6 +3,7 @@ package com.bsf.wallet.integration.service.impl;
 import com.bsf.wallet.dto.response.AccountDetail;
 import com.bsf.wallet.entity.Account;
 import com.bsf.wallet.repository.AccountRepository;
+import com.bsf.wallet.repository.TransactionRepository;
 import com.bsf.wallet.service.impl.AccountServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,12 +32,17 @@ class AccountServiceImplTest {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+
     @BeforeEach
     void setUp() {
     }
 
     @Test
     void getAccounts() {
+        transactionRepository.deleteAll(); // to cascade delete
+        accountRepository.deleteAll();
         List<Account> accountsSaved = Arrays.asList(
                 new Account(1L, "John", "323232", BigDecimal.TEN, LocalDateTime.now()),
                 new Account(2L, "Anna", "898787", BigDecimal.TEN, LocalDateTime.now())
